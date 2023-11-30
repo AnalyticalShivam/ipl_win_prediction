2a7cfe8487303dcd23c065abdc7ecc42f72ac52d
AnalyticalShivam <ssg98670@gmail.com>
2023-11-30 08:37:51 +0530

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 import pickle
 
-
 # Declaring the teams
 
 
@@ -23,7 +22,6 @@ teams = ['Sunrisers Hyderabad',
 
          'Delhi Capitals']
 
-
 # declaring the venues
 
 
@@ -39,71 +37,51 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
 
           'Sharjah', 'Mohali', 'Bengaluru']
 
-
-
 pipe = pickle.load(open('pipe.pkl', 'rb'))
 
 st.title('IPL Win Predictor')
 
-
-
 col1, col2 = st.columns(2)
 
-
 with col1:
-
     battingteam = st.selectbox('Select the batting team', sorted(teams))
 
-
 with col2:
-
-
     bowlingteam = st.selectbox('Select the bowling team', sorted(teams))
-
-
 
 city = st.selectbox(
 
     'Select the city where the match is being played', sorted(cities))
 
-
-
 target = st.number_input('Target')
-
 
 col3, col4, col5 = st.columns(3)
 
-
 with col3:
-
     score = st.number_input('Score')
 
-
 with col4:
-
     overs = st.number_input('Overs Completed')
 
-
 with col5:
-
     wickets = st.number_input('Wickets Fallen')
 
-
-
 if st.button('Predict Probability'):
-    runs_left = target-score
-    balls_left = 120-(overs*6)
-    wickets = 10-wickets
+    runs_left = target - score
+    balls_left = 120 - (overs * 6)
+    wickets = 10 - wickets
     if overs != 0:
-        currentrunrate = score/overs
+        currentrunrate = score / overs
     else:
         currentrunrate = 0
-    requiredrunrate = (runs_left*6)/balls_left
-    
-    input_df = pd.DataFrame({'batting_team': [battingteam], 'bowling_team': [bowlingteam], 'city': [city], 'runs_left': [runs_left], 'balls_left': [
+    requiredrunrate = (runs_left * 6) / balls_left
 
-                            balls_left], 'wickets': [wickets], 'total_runs_x': [target], 'cur_run_rate': [currentrunrate], 'req_run_rate': [requiredrunrate]})
+    input_df = pd.DataFrame(
+        {'batting_team': [battingteam], 'bowling_team': [bowlingteam], 'city': [city], 'runs_left': [runs_left],
+         'balls_left': [
 
+             balls_left], 'wickets': [wickets], 'total_runs_x': [target], 'cur_run_rate': [currentrunrate],
+         'req_run_rate': [requiredrunrate]})
 
     result = pipe.predict_proba(input_df)
 
@@ -111,9 +89,6 @@ if st.button('Predict Probability'):
 
     winprob = result[0][1]
 
+    st.header(battingteam + "- " + str(round(winprob * 100)) + "%")
 
-    st.header(battingteam+"- "+str(round(winprob*100))+"%")
-
-
-    st.header(bowlingteam+"- "+str(round(lossprob*100))+"%")
-
+    st.header(bowlingteam + "- " + str(round(lossprob * 100)) + "%")
